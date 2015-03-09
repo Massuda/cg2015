c52506c2a555d4372ae09c292a06a12cbd381052
Massuda <luca.massuda@gmail.com>
2015-03-09 21:45:53 +0100

    es3

---
+++ 20150309/exercise03.js
@@ -0,0 +1,11 @@
+//write a script containing the function fibonacci(i)
+//that returns the i-th element of the Fibonacci's serie 
+//(apply memoization pattern)
+function fibonacci(i) {
+	if (!(i in fibonacci)) {
+		fibonacci[i] = fibonacci(i-1) + fibonacci(i-2);
+	}
+  return fibonacci[i];
+}
+fibonacci[0] = 1;
+fibonacci[1] = 1;
